01d63d0ddd2eb5c8ff96de57b69046b22876dbe6
Piyush Jain <piyushjain76296@gmail.com>
2026-04-02 23:14:40 +0530

    final submission - finance backend

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,13 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    const conn = await mongoose.connect(process.env.MONGO_URI as string, {
+      serverApi: {
+        version: '1',
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     if (error instanceof Error) {
